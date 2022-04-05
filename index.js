14d3998875e9b3c6938c99e0e4d98494415f1d3d
irufano <irfanhidayatms@gmail.com>
2022-04-05 15:47:47 +0700

    setup socket.io

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const express = require("express");
+const server = require('http').createServer()
+const socketIO = require('socket.io')(server)
 
-// const server = require('http').createServer()
+socketIO.on('connection', function (client) {
+  console.log('Connected...', client.id);
+
+  client.on('message', function name(data) {
+    console.log(data);
+    socketIO.emit('message', data);
+  })
+
+  client.on('disconnect', function () {
+    console.log('Disconnected...', client.id);
+  })
+
+  client.on('error', function (err) {
+    console.log('Error detected', client.id);
+    console.log(err);
+  })
+})
+
 var port = process.env.PORT;
 
-const app = express();
-const server = app.listen(port, function (err) {
+server.listen(port, function (err) {
   if (err) console.log(err);
-  console.log("Listening on port", port);
+  console.log('Listening on port', port);
 });
-const socketIO = require("socket.io")(server);
-
-socketIO.on("connection", function (client) {
-  console.log("Connected...", client.id);
-
-  client.on("message", function name(data) {
-    console.log(data);
-    socketIO.emit("message", data);
-  });
-
-  client.on("disconnect", function () {
-    console.log("Disconnected...", client.id);
-  });
-
-  client.on("error", function (err) {
-    console.log("Error detected", client.id);
-    console.log(err);
-  });
-});
-
-
-// server.listen(port, function (err) {
-//   if (err) console.log(err);
-//   console.log("Listening on port", port);
-// });
